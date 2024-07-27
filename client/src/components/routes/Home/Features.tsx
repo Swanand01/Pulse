1d0846c97d4a96765b5eb8fb4de93a6eeef2f082
Swanand01 <swanandmathekar@gmail.com>
2024-07-27 17:39:21 +0530

    Add Home route and its components

---
+++ client/src/components/routes/Home/Features.tsx
@@ -0,0 +1,35 @@
+import { CheckCircle, Globe, Lock } from "lucide-react";
+
+const FEATURES = [
+  {
+    icon: <CheckCircle />,
+    heading: "Easy to Use",
+    text: "No login or sign-up needed. Open the web app and start sharing instantly!",
+  },
+  {
+    icon: <Lock />,
+    heading: "Secure",
+    text: "Your files are transferred browser to browser, and never stored on the server.",
+  },
+  {
+    icon: <Globe />,
+    heading: "Anywhere",
+    text: "Effortlessly share files across any device, no matter where you are in the world.",
+  },
+];
+
+export default function Features() {
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 px-8 prose dark:prose-invert">
+      {FEATURES.map(({ icon, heading, text }) => (
+        <div className="flex flex-col max-w-80 gap-4" key={heading}>
+          <div className="flex gap-2 items-center">
+            {icon}
+            <h3 className="m-0">{heading}</h3>
+          </div>
+          <p className="my-0">{text}</p>
+        </div>
+      ))}
+    </div>
+  );
+}

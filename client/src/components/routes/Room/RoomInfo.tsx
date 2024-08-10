5bb774b0103eef4f4c1d37c9fe734179f62b2aa7
Swanand01 <swanandmathekar@gmail.com>
2024-08-10 16:25:27 +0530

    Delete Peers.tsx

--- client/src/components/routes/Room/RoomInfo.tsx
+++ client/src/components/routes/Room/RoomInfo.tsx
@@ -1,18 +1,15 @@
 import { cn } from "@/lib/utils";
 import ConnectionStatus from "./ConnectionStatus";
-import Peers from "./Peers";
 import TransferSpeed from "./TransferSpeed";
 
 interface RoomInfoProps {
   connectionStatus: string;
-  numberOfPeers: number;
   transferSpeed: string;
   className?: string;
 }
 
 export default function RoomInfo({
   connectionStatus,
-  numberOfPeers,
   transferSpeed,
   className,
 }: RoomInfoProps) {
@@ -25,7 +22,6 @@ export default function RoomInfo({
     >
       <ConnectionStatus connectionStatus={connectionStatus} />
       <div className="flex gap-4 justify-center">
-        <Peers numberOfPeers={numberOfPeers} />
         <TransferSpeed transferSpeed={transferSpeed} />
       </div>
     </div>

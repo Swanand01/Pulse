5fde1b5d46844f18d517e9ab8ee1b057a9ac6cda
Swanand01 <swanandmathekar@gmail.com>
2024-08-10 16:24:55 +0530

    Add Setup route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
 import Home from "./components/routes/Home";
 import Room from "./components/routes/Room";
+import Setup from "./components/routes/Setup";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/setup" element={<Setup />} />
         <Route path="/:roomId" element={<Room />} />
       </Routes>
       <Footer />
---
+++ client/src/components/routes/Setup/index.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+export default function Setup() {
+  const [username, setUsername] = useLocalStorage("username", "");
+  const [searchParams] = useSearchParams();
+  const roomId = searchParams.get("roomId");
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      usernameInput: HTMLInputElement;
+    };
+    const usernameInputValue = formElements.usernameInput.value;
+
+    setUsername(usernameInputValue);
+
+    if (roomId) {
+      navigate(`/${roomId}`, { replace: true });
+    }
+  }
+
+  return (
+    <div className="w-full px-8 sm:w-96 sm:px-0 m-auto space-y-8">
+      <Card className="w-fit">
+        <CardHeader className="prose prose-invert">
+          <CardTitle>Choose a Nickname</CardTitle>
+          <CardDescription>
+            Nicknames are used to identify devices in a room.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+            <Input
+              id="usernameInput"
+              placeholder="Cool nickname"
+              maxLength={20}
+              defaultValue={username}
+              required
+            />
+            <Button className="w-fit self-end" type="submit">
+              Save
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}

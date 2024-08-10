9762999f9f06e56287c13b2940d31f2163ea4368
Swanand01 <swanandmathekar@gmail.com>
2024-08-10 16:27:43 +0530

    Add UsernameTakenDialog component

---
+++ client/src/components/routes/Room/UsernameTakenDialog.tsx
@@ -0,0 +1,49 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useNavigate } from "react-router-dom";
+
+interface DownloadDialogProps {
+  open: boolean;
+  username: string;
+  roomId: string;
+}
+
+export default function UsernameTakenDialog({
+  open,
+  username,
+  roomId,
+}: DownloadDialogProps) {
+  const navigate = useNavigate();
+
+  return (
+    <AlertDialog open={open}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Username Taken</AlertDialogTitle>
+          <AlertDialogDescription>
+            Someone with the username {username} is already present in the room.
+            Do you want to change your username?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => navigate("/")}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => navigate(`/setup?roomId=${roomId}`)}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}

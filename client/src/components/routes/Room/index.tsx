43a0a8add7310e09de51a59797c54283f60a1525
Swanand01 <swanandmathekar@gmail.com>
2024-08-25 16:12:25 +0530

    Update Room route

--- client/src/components/routes/Room/index.tsx
+++ client/src/components/routes/Room/index.tsx
@@ -1,15 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useFileSharing } from "@/hooks/useFileSharing";
-import ShareLink from "./ShareLink";
+import ShareLinkAlert from "./ShareLink/ShareLinkAlert";
 import DownloadDialog from "./DownloadDialog";
 import SendFileButton from "./SendFileButton";
 import RoomInfo from "./RoomInfo";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import UsernameTakenDialog from "./UsernameTakenDialog";
+import NetworkGraph from "./NetworkGraph";
+import { ShareLinkDialog } from "./ShareLink/ShareLinkDialog";
 
 export default function Room() {
   const { roomId } = useParams<{ roomId: string }>();
   const [username] = useLocalStorage("username", "");
+  const navigate = useNavigate();
 
   const {
     connectionStatus,
@@ -23,22 +27,27 @@ export default function Room() {
     peers,
   } = useFileSharing({ roomId: roomId || "", username });
 
+  useEffect(() => {
+    if (!username) {
+      navigate(`/setup?roomId=${roomId}`);
+    }
+  }, [navigate, roomId, username]);
+
   return (
-    <div className="w-full px-8 sm:w-96 sm:px-0 m-auto space-y-8">
-      {username}
-      <br />
-      peers:{peers}
+    <div className="w-full px-8 sm:w-[500px] sm:px-0 m-auto space-y-8">
+      <NetworkGraph users={[username, ...peers]} />
       <RoomInfo
         connectionStatus={connectionStatus}
         transferSpeed={transferSpeed}
         className={connectionStatus === "" ? "hidden" : "block"}
       />
-      <ShareLink
-        className={`transition-transform duration-500 ${
-          connectionStatus !== "" ? "translate-y-4" : "translate-y-0"
-        }`}
-      />
-      <div className="absolute bottom-16 right-10">
+      {connectionStatus === "" && <ShareLinkAlert />}
+      {connectionStatus !== "" && (
+        <div className="absolute bottom-16 left-8 sm:left-10">
+          <ShareLinkDialog />
+        </div>
+      )}
+      <div className="absolute bottom-16 right-8 sm:right-10">
         <SendFileButton
           sendFile={sendFile}
           disabled={connectionStatus === ""}
